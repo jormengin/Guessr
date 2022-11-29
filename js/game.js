3dbb378cd459e2d46f18d4b1c84cc50d68f4d0b7
Jorge Mendez <jorge.mendezgines@gmail.com>
2022-11-29 20:29:28 +0100

    start table

--- js/game.js
+++ js/game.js
@@ -3,12 +3,11 @@ class Game {
     this.secretNumber = [];
     this.maxAttempts = maxAttempts;
     this.userAttempts = 0;
-    // userScore
-    this.UserScore = 0;
+    this.userScore = 0;
+    this.id = 1;
   }
 
   start() {
-    // leave this only in script.js and move the call to generaterandomnumber here
     console.log("holi start");
     this._generateRandomNumber();
     document.getElementById("start-page").style = "display: none";
@@ -16,46 +15,43 @@ class Game {
 
   checkAttempt() {
     this.userAttempts++;
-    let rightGuesses = 0;
+    let mates = 0;
+    let checks = 0;
     const guesses = document
       .getElementsByClassName("guess-input")[0]
       .value.split("");
     const guessArr = guesses.map((str) => {
       return Number(str);
     });
-    console.log(guesses);
-    console.log(guessArr);
-    guessArr.push(parseInt(guesses));
     if (guesses.length !== 4) {
       alert("Introduce a valid number!");
+      return
     }
     for (let i = 0; i < guessArr.length; i++) {
       if (
         this.secretNumber.includes(guessArr[i]) &&
         this.secretNumber[i] === guessArr[i]
       ) {
-        rightGuesses++;
-        console.log(
-          `Number ${guessArr[i]} is right and is in the right position 🟢`
-        );
+        mates++;
       } else if (this.secretNumber.includes(guessArr[i])) {
-        console.log(
-          `Number ${guessArr[i]} is right but in the wrong position 🟠`
-        );
-      } else {
-        console.log(
-          `Number ${guessArr[i]} is not one of the secret numbers 🔴`
-        );
-      }
+        checks++;
+      } 
+      console.log(`Mates:${mates}, Checks:${checks}`);
     }
-    if (rightGuesses === 4) {
+    if (mates === 4) {
       alert("Omg you win 🎉🎉!");
     }
-
+    this._createNewRow();
     document.getElementById("attempts-user").innerHTML = this.userAttempts;
     this._checkIfLost();
   }
-
+  _createNewRow() {
+  id = id + 1;
+  const newRow = document.createElement('tr');
+  newRow.id = id;
+  newRow.innerHTML = `Mates:${mates}, Checks:${checks}`
+  tbody.appendChild(newRow);
+}
   _generateRandomNumber() {
     const arrNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     for (let i = 0; i < 4; i++) {
@@ -65,12 +61,10 @@ class Game {
       console.log(this.secretNumber);
     }
   }
-
   _checkIfLost() {
     if (this.userAttempts === 10) {
-      //
-      document.getElementById("main-page").classList.add("hidden");
-      document.getElementById("lose-page").classList.toggle("shown");
+      document.getElementById("main-page").style = "display: none";
+      document.getElementById("lose-page").style = "display:flex";
       alert(`YOU LOSE!. The secret number was ${this.secretNumber.join("")}`);
     }
   }
