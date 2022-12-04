43d58e0ead372ca8e9024a19f7aab8b3f06dd9af
Jorge Mendez <jorge.mendezgines@gmail.com>
2022-12-04 18:36:33 +0100

    async print function works

--- js/wordl.js
+++ js/wordl.js
@@ -11,63 +11,63 @@ class Wordle extends Game {
     this.numberCorrectGuesses = 0;
     this.userWordToPrint = [];
   }
-   checkWord() {
-      this.numberCorrectGuesses = 0;
-      this.userWord = document.getElementsByClassName("word-input")[0].value.toUpperCase();
-      if (this.userWord.length !== 5 || typeof this.userWord !== "string" ||!wordsArray.includes(this.userWord.toLocaleLowerCase())) {
-        alert("Introduce a valid Word!");
-        return
-      }
-      this.userWordArr = this.userWord.split("");
-      let row =
-        document.getElementsByClassName("letter-row")[6 - this.numberOfGuesses];
-      this._insertWord();
-      this.numberOfGuesses--;
-     for (let i = 0; i < this.userWordArr.length; i++) {
-       let letterObject = {};
-        let box = row.children[i];
-        let letterCheck = this.secretWordArr.indexOf(this.userWordArr[i]);
-       if (letterCheck === -1) {
-          letterObject = {
-            letter: this.userWordArr[i],
-            color: "incorrect-letter-box",
-          };
-          this.userWordToPrint.push(letterObject)
-          box.classList.add("incorrect-letter-box");
+  checkWord() {
+    this.userWordToPrint = [];
+    this.numberCorrectGuesses = 0;
+    this.userWord = document
+      .getElementsByClassName("word-input")[0]
+      .value.toUpperCase();
+    if (
+      this.userWord.length !== 5 ||
+      typeof this.userWord !== "string" ||
+      !wordsArray.includes(this.userWord.toLocaleLowerCase())
+    ) {
+      alert("Introduce a valid Word!");
+      return;
+    }
+    this.userWordArr = this.userWord.split("");
+    for (let i = 0; i < this.userWordArr.length; i++) {
+      let letterObject = {};
+      let letterCheck = this.secretWordArr.indexOf(this.userWordArr[i]);
+      if (letterCheck === -1) {
+        letterObject.letter = this.userWordArr[i];
+        letterObject.color = "incorrect-letter-box";
+        this.userWordToPrint.push(letterObject);
+      } else {
+        if (this.secretWordArr[i] === this.userWordArr[i]) {
+          letterObject.letter = this.userWordArr[i];
+          letterObject.color = "correct-letter-box";
+          this.userWordToPrint.push(letterObject);
+          this.numberCorrectGuesses++;
         } else {
-         if (this.secretWordArr[i] === this.userWordArr[i]) {
-            letterObject = {
-              letter: this.userWordArr[i],
-              color: "correct-letter-box",
-            };
-            box.classList.add("correct-letter-box");
-            this.numberCorrectGuesses++;
-         } else {
-           letterObject = {
-             letter: this.userWordArr[i],
-             color: "almost-letter-box",
-           };
-            box.classList.add("almost-letter-box");
-          }
-       }
-       console.log(this.userWordToPrint);
-       this._checkIfWinWords();
-       
-     }
-      this._checkIfLostWords();
+          letterObject.letter = this.userWordArr[i];
+          letterObject.color = "almost-letter-box";
+          this.userWordToPrint.push(letterObject);
+        }
+      }
+ this._asyncPrinter();
+      this._checkIfWinWords();
     }
-   _insertWord() {
-      this.letterPosition = 0;
-      let row =
-        document.getElementsByClassName("letter-row")[6 - this.numberOfGuesses];
-        this.userWordArr.map(x => {
-          console.log('for loop insert word funca');
-          let box = row.children[this.letterPosition];
-          box.innerHTML = x;
-          box.classList.add("filled-box");
-          this.letterPosition++;
-        })
+   
+    this.numberOfGuesses--;
+    this._checkIfLostWords();
+  }
+
+  _asyncPrinter() {
+    console.log(`this is the number of guesses ${this.numberOfGuesses} at async printer`);
+     let row =
+       document.getElementsByClassName("letter-row")[6 - this.numberOfGuesses];
+    for (let i = 0; i < this.userWordToPrint.length; i++) {
+      let delay = 200 * i;
+      setTimeout(() => {
+        let box = row.children[i];   
+        box.innerHTML = this.userWordToPrint[i].letter;
+        box.classList.add(this.userWordToPrint[i].color);
+        console.log(box.classList);
+      }, delay);
     }
+  }
+
   _pickRandomWord() {
     const randomIndex = Math.floor(Math.random() * wordsArray.length);
     this.secretWord = wordsArray[randomIndex].toUpperCase();
@@ -75,19 +75,17 @@ class Wordle extends Game {
     console.log(this.secretWord);
   }
   _checkIfWinWords() {
-    console.log(
-      `the number of correct guesses is ${this.numberCorrectGuesses} before check if lost`
-    );
     if (this.numberCorrectGuesses === 5) {
-      document.getElementById("wordl-page").style = "display: none";
-      document.getElementById("win-page").style = "display:flex";
-      this.wordStart();
-      return;
+      setTimeout(() => {
+        document.getElementById("wordl-page").style = "display: none";
+        document.getElementById("win-page").style = "display:flex";
+        this.wordStart();
+        return;
+      },2200)
     }
   }
   _checkIfLostWords() {
-    console.log('im checkiflost making your life miserable');
-    if (this.numberOfGuesses === 0 && this.numberCorrectGuesses!==5) {
+    if (this.numberOfGuesses === 0 && this.numberCorrectGuesses !== 5) {
       document.getElementById("wordl-page").style = "display: none";
       document.getElementById("lose-page").style = "display:flex";
       this.wordStart();
@@ -133,9 +131,21 @@ class Wordle extends Game {
 //  _insertWord() {
 //     this.letterPosition = 0;
 //     let row =
-//       document.getElementsByClassName("letter-row")[6 - this.numberOfGuesses]; 
+//       document.getElementsByClassName("letter-row")[6 - this.numberOfGuesses];
 //       this.userWordArr.map(x => {
 //         console.log('for loop insert word funca');
+//         let box = row.children[this.letterPosition];
+//         box.innerHTML = x;
+//         box.classList.add("filled-box");
+//         this.letterPosition++;
+//       })
+//   }
+
+//  _insertWord() {
+//     this.letterPosition = 0;
+//     let row =
+//       document.getElementsByClassName("letter-row")[6 - this.numberOfGuesses];
+//       this.userWordArr.map(x => {
 //         let box = row.children[this.letterPosition];
 //         box.innerHTML = x;
 //         box.classList.add("filled-box");
